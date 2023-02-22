0e1c57fae9ad227235f7bdb194ce12da836575b6
Mohamed Reda Daghouj <mohamedredadaghouj@gmail.com>
2023-02-22 02:30:10 +0100

    first commit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Slider
 const sliderWrapper = document.querySelector(".slider .wrapper");
 const prevBtn = document.querySelector(".controls .prev");
 const nextBtn = document.querySelector(".controls .next");
@@ -13,4 +14,15 @@ nextBtn.addEventListener("click", () => {
   i--;
   if (i < 0) i = 3;
   sliderWrapper.style.marginRight = `-${i * 270}px`;
+});
+
+// Toggler Cities
+
+const togglerBtn = document.querySelector(".toggler");
+const cities = document.querySelector(".cities");
+const nav = document.querySelector("header nav ul");
+
+togglerBtn.addEventListener("click", () => {
+  cities.classList.toggle("active");
+  nav.classList.toggle("hidden");
 });
